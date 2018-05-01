80512864691a59a21e901ec286b709db34b05534
Kay Mok <kaymok3@gmail.com>
2018-05-01 16:37:03 -0400

    Generate controllers

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,40 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/create'
+  get 'users/show'
+  get 'users/destroy'
+  get 'users/update'
+  get 'profiles/index'
+  get 'profiles/update'
+  get 'instructors/index'
+  get 'instructors/create'
+  get 'instructors/show'
+  get 'instructors/edit'
+  get 'instructors/destroy'
+  get 'instructors/update'
+  get 'students/index'
+  get 'students/create'
+  get 'students/show'
+  get 'students/edit'
+  get 'students/destroy'
+  get 'students/update'
+  get 'cohorts/index'
+  get 'cohorts/create'
+  get 'cohorts/show'
+  get 'cohorts/edit'
+  get 'cohorts/destroy'
+  get 'cohorts/update'
+  get 'courses/index'
+  get 'courses/create'
+  get 'courses/show'
+  get 'courses/edit'
+  get 'courses/destroy'
+  get 'courses/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :courses
+  resources :cohorts
+  resources :instructors
+  resources :students
+  resources :profiles
+  resources :users
 end
