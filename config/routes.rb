14dc76902a9f0087f876c09809351725405e973b
Kay Mok <kaymok3@gmail.com>
2018-05-02 10:54:10 -0400

    Create nesting routes

--- config/routes.rb
+++ config/routes.rb
@@ -31,8 +31,9 @@ Rails.application.routes.draw do
   get 'courses/destroy'
   get 'courses/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :courses
-  resources :cohorts
+  resources :courses do
+    resources :cohorts
+  end
   resources :instructors
   resources :students
   resources :profiles
