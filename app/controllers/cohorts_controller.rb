6c7e38486ca8fe424436f2693f1e3140405ec1c2
Kay Mok <kaymok3@gmail.com>
2018-05-03 19:49:55 -0400

    Add index view for courses

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,25 +1,44 @@
 class CohortsController < ApplicationController
   # links to /cohorts route
   def index
-    @kay = Admin.first
+    @cohorts = Cohort.all
   end
 
   # links to /cohorts/:id route
   def show
+    @cohort = Cohort.find(params[:id])
   end
 
   def new
+    @cohort = Cohort.new
   end
 
   def create
+    Cohort.create(cohort_params)
+    # change link to nested
+    redirect_to '/cohorts'
   end
 
   def edit
+    @cohort = Cohort.find(params[:id])
   end
 
   def update
+    @cohort = Cohort.find(params[:id])
+    @cohort.update(cohort_params)
+    # change link to nested
+    redirect_to '/cohorts'
   end
 
   def destroy
+    @cohort = Cohort.find(params[:id]).destroy
+    # change link to nested
+    redirect_to '/cohorts'
+  end
+
+  private
+
+  def cohort_params
+    params.require(:owner).permit(:name, :start_date, :end_date, :course_id, :instructor_id)
   end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,22 +1,40 @@
 class CoursesController < ApplicationController
+
   def index
+    @courses = Course.all
   end
 
   def show
+    @course = Course.find(params[:id])
   end
 
   def new
+    @course = Course.new
   end
 
   def create
+    Course.create(course_params)
+    redirect_to '/cohorts'
   end
 
   def edit
+    @cohort = Cohort.find(params[:id])
   end
 
   def update
+    @cohort = Cohort.find(params[:id])
+    @cohort.update(cohort_params)
+    redirect_to '/cohorts'
   end
 
   def destroy
+    @cohort = Cohort.find(params[:id]).destroy
+    redirect_to '/cohorts'
+  end
+
+  private
+
+  def cohort_params
+    params.require(:owner).permit(:name, :start_date, :end_date, :course_id, :instructor_id)
   end
 end
