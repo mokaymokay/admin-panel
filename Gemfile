84ed08ce0fc11e5d015f1b7faaf3d1e922a17fa9
Kay Mok <kaymok3@gmail.com>
2018-05-04 13:59:45 -0400

    Use Bootstrap gem

--- Gemfile
+++ Gemfile
@@ -37,7 +37,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 # CSS
-gem 'bootstrap-sass'
+gem 'sprockets-rails', :require => 'sprockets/railtie'
+gem 'bootstrap', '~> 4.1.1'
 gem 'font-awesome-rails'
 # JS
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( bootstrap.min.css font-awesome.min.css jquery.min.js bootstrap.bundle.min.js jquery.easing.min.js )
+Rails.application.config.assets.precompile += %w(  )
