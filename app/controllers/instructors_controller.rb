eb2c38889e384d5c444633bfe06b7989408886a3
Kay Mok <kaymok3@gmail.com>
2018-05-15 22:17:51 -0400

    Add student to cohort form and all button styling

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -23,8 +23,6 @@ class InstructorsController < ApplicationController
         format.html { redirect_to instructors_path }
         format.js
       }
-    # else
-      # flash[:error] = @instructor.errors.full_messages.first
     end
   end
 
